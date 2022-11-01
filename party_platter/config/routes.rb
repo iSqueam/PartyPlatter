152629336228073fff3ca7ff9c5101e2759bdec2
iSqueam <sclarbear@gmail.com>
2022-11-01 05:38:30 -0400

    moved react app folder into top level of backend folder

--- party_platter/config/routes.rb
+++ party_platter/config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       devise_for :users
       resources :characters, :campaigns, only: [:create, :show, :update, :destroy, :index]
-      #resources :campaign, only: [:create, :show, :update, :destroy, :index]
     end
   end
 end
