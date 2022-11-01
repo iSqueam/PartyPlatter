85481033b016d1ee5321b94b4235a57e8e8b7354
iSqueam <sclarbear@gmail.com>
2022-10-31 23:02:03 -0400

    Validation for campaign and character classes

--- party_platter/app/models/campaign.rb
+++ party_platter/app/models/campaign.rb
@@ -1,2 +1,5 @@
 class Campaign < ApplicationRecord
+    validates :name, length: {minimum: 2}
+    validates :minplayers, numericality: {only_integer: true, minimum: 0}
+    validates :maxplayers, numericality: {only_integer: true, minimum: :minplayers}
 end
--- party_platter/app/models/character.rb
+++ party_platter/app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+    validates :name, length: {minimum: 2}
+    validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: {only_integer: true, in: 3..20}
 end
